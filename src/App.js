adedc51e657c732f23954ad1ee01d81d1b5bd3c0
Thanh <nmthanh206@gmail.com>
2021-04-23 13:28:39 +0700

    gan xong

--- src/App.js
+++ src/App.js
@@ -10,21 +10,7 @@ import { createMarker } from "./Map";
 import { popupFormat } from "./Components/Search/SearchCenter";
 import { getWayPointsArray } from "./Ulti/getWayPointsArray";
 import MapSetUp from "./Components/Map/MapSetUp";
-// import axios from "axios";
-// const ps = [
-//   [10.841172501968856, 106.75928732628947],
-//   [10.847944564456817, 106.76160644370741],
-//   [10.847944364456817, 106.76160644370741],
-//   [10.847944562456817, 106.76160644370741],
-//   [10.827944564456817, 106.76150644370741],
-// ];
 
-// const costMatrix = [
-//   [0, 1, 3, 4],
-//   [1, 0, 2, 3],
-//   [3, 2, 0, 5],
-//   [4, 3, 5, 0],
-// ];
 // "start": "set PORT=3006 && react-scripts start"
 const wp = control
   .getPlan()
@@ -55,15 +41,8 @@ function App(props) {
     control.getPlan().options.createMarker = createMarker(setPoints);
     map.locate();
   };
-  // const onSave = e => {
-  //   e.preventDefault();
-  //   alert("ok save");
-  // };
   return (
     <>
-      {/* <button className="btn-save" onClick={onSave}>
-        SAVE
-      </button> */}
       <div className="map-bar">
         <MapSetUp
           center={position}
@@ -79,7 +58,6 @@ function App(props) {
             className="toggle-btn"
             onClick={() => {
               if (flag) {
-                // console.log(control.hide);
                 control.hide();
                 flag = false;
               } else {
